baab909ca5642036e0c828114605c92b1430c906
Gabriel Cornejo <gcornejo441@gmail.com>
2023-05-03 17:03:59 -0700

    adding projectracker

---
+++ ManageEmployees/ManageEmployees/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Util;
+
+namespace ManageEmployees.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            View();
+            // Build the JavaScript code to be returned
+            var jsCode = $"alert('My data is');";
+
+            // Use the Controller.Javascript() method to create a JavaScriptResult object
+            return JavaScript(jsCode);
+
+        }
+
+        
+
+        //
+        // GET: /HelloWorld/Welcome
+        public string Welcome(string name, int numTimes = 1)
+        {
+        
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
---
+++ ManageEmployees/ManageEmployees/Controllers/ProjectTrackerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManageEmployees.Controllers
+{
+    public class ProjectTrackerController : Controller
+    {
+        // GET: ProjectTracker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
